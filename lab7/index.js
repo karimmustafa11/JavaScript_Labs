8682bceeb83b738c2f1c60201160ec4e86712148
karimmustafa <karimmustafa495@gmail.com>
2025-02-07 13:42:02 +0200

    modifing Lab7

--- lab7/index.js
+++ lab7/index.js
@@ -1,6 +1,7 @@
 var linkedList = {
     data: [],
 
+    // enqueue funcion
     Enqueue: function (value) {
         if (this.data.length > 0 && value >= this.data[0].val) {
             throw new Error(`Please enter a value smaller than ${this.data[0].val}`);
@@ -8,12 +9,12 @@ var linkedList = {
         this.data.unshift({ val: value });
     },
 
+    // dequeue funcion
     dequeue: function () {
         if (this.data.length == 0) {
             alert("List is empty.")
         }
         var removed = this.data[0]
-        // Shift elements 
         for (let i = 0; i < this.data.length - 1; i++) {
             this.data[i] = this.data[i + 1]
         }
@@ -21,6 +22,62 @@ var linkedList = {
         return removed
     },
 
+    //push function
+    push: function (value) {
+        // Check if the value is in sequence
+        if (this.data.length > 0 && value <= this.data[this.data.length - 1].val) {
+            throw new Error(`Please enter a value Bigger than ${this.data[this.data.length - 1].val}`);
+        }
+        this.data.push({ val: value })
+    },
+
+    //pop function 
+    pop() {
+        if (this.data.length == 0) {
+            throw new Error("List is empty.")
+        }
+        const removed = this.data[this.data.length - 1]
+        this.data.length -= 1
+        return removed
+    },
+
+    //insert into spesific index
+    insertAt: function (index, value) {
+        for (let i = 0; i < this.data.length; i++) {
+            if (this.data[i].val == value) {
+                throw new Error("Duplicate values are not allowed.");
+            }
+        }
+
+        if (index < 0 || index > this.data.length) {
+            throw new Error("Invalid index.");
+        }
+
+        for (var i = this.data.length; i > index; i--) {
+            this.data[i] = this.data[i - 1];
+        }
+
+        this.data[index] = { val: value };
+    },
+
+
+    //remove specific value
+    remove(value) {
+        const index = this.data.indexOf(value);
+        if (index === -1) {
+            return "Data not found.";
+        }
+        this.data.splice(index, 1);
+    },
+
+    //display the list
+    display() {
+        const result = []
+        for (let i = 0; i < this.data.length; i++) {
+            result.push(this.data[i].val);
+        }
+        console.log(result);
+    }
 
 }
 
